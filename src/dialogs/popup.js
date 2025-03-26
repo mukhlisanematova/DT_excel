c00ad75391be519471847b72452d3b010cb15fc0
mukhlisanematova <mukhlisanematova>
2025-03-26 08:34:54 -0400

    initial commit

---
+++ src/dialogs/popup.js
@@ -0,0 +1,9 @@
+(async () => {
+  await Office.onReady();
+
+  document.getElementById("ok-button").onclick = sendStringToParentPage;
+  function sendStringToParentPage() {
+    const userName = document.getElementById("name-box").value;
+    Office.context.ui.messageParent(userName);
+  }
+})();
